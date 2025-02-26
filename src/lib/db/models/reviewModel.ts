dbf01b5aa9c1bee6694d28079ae7f161c35a4f6c
Paing4M <paing.2000@yahoo.com>
2025-02-26 10:16:29 +0630

    Create product review schema

---
+++ src/lib/db/models/reviewModel.ts
@@ -0,0 +1,33 @@
+import {model, models, Schema, Types} from "mongoose";
+
+
+export interface ReviewInterface extends Document {
+  _id: string;
+  name: string;
+  rating: number;
+  comment: string;
+  user: Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+
+const reviewSchema = new Schema<ReviewInterface>({
+  rating: {
+    type: Number,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+
+}, {timestamps: true});
+
+const Review = models.Review || model<ReviewInterface>('Review', reviewSchema);
+
