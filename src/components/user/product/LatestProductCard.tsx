101112074741c3fe67ae11eebf86f79b21f6caec
Paing4M <paing.2000@yahoo.com>
2025-03-04 09:10:00 +0630

    Modify product link

--- src/components/user/product/LatestProductCard.tsx
+++ src/components/user/product/LatestProductCard.tsx
@@ -12,10 +12,10 @@ const LatestProductCard = ({product, className}: { product: ProductShemaI, class
       <div className='absolute z-[2] inset-0 bg-black/70'/>
 
       <div className='absolute bottom-[2%] left-[3%] right-[3%] p-2 z-[3] w-[80%]'>
-        <Link href={`/src/app/(user)/products/${product.slug}`}
+        <Link href={`/products/${product.slug}`}
               className='text-lg text-gray-300 font-semibold'>{product.name}</Link>
         <p className='text-sm w-[90%] text-gray-300 line-clamp-2'>{product.description}</p>
-        <Link href={`/src/app/(user)/products/${product.slug}`}
+        <Link href={`/products/${product.slug}`}
               className='text-gray-300 font-semibold underline decoration-redBackground'>Shop Now</Link>
       </div>
     </div>
