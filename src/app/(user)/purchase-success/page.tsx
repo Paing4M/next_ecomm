775bf1f866d9e648f3feb2aed0d31beaa81a8af5
Paing4M <paing.2000@yahoo.com>
2025-05-29 15:11:54 +0630

    fix bug

--- src/app/(user)/purchase-success/page.tsx
+++ src/app/(user)/purchase-success/page.tsx
@@ -9,7 +9,7 @@ const PurchaseSuccessPage = async () => {
 
   return (
     <section className="flex items-center justify-center h-[calc(100vh-70px)]">
-      <Suspense fallback={<p className="text-center p-8">Loading...</p>}>
+      <Suspense>
         <SuccessPurchase/>
       </Suspense>
     </section>
